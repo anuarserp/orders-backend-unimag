8685fb979f7e5198df957dd1754e782c199e32c6
Anuar Sepulveda <anuarserp@protonmail.com>
2022-10-09 19:59:04 +0000

    deploy

--- src/infrastructure/driving-adapters/index.ts
+++ src/infrastructure/driving-adapters/index.ts
@@ -1,13 +1,8 @@
 import 'module-alias/register'
-import path from 'path'
-import * as dotenv from 'dotenv'
+import 'dotenv/config'
 import { BackendApp } from './rest-api/BackendApp'
 
 try {
-   dotenv.config({
-      path: path.resolve(__dirname, '../../../../.env')
-   })
-
    new BackendApp().start()
 } catch (error) {
    console.log(error)
